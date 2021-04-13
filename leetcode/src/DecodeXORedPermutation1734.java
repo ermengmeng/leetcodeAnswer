55f66f17aa262623b20c3f917157e7beb4b8aa13
ouyangyameng <ouyangyameng@vip.qq.com>
2021-04-13 12:51:15 +0800

    1734.DecodeXORedPermutation

---
+++ leetcode/src/DecodeXORedPermutation1734.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+/**
+ * @Author: ouyangyameng
+ * @Date: 2021/4/13 12:09 下午
+ * @Version 1.0
+ */
+public class DecodeXORedPermutation1734 {
+    public static void main(String[] args) {
+        DecodeXORedPermutation1734 o = new DecodeXORedPermutation1734();
+        System.out.print(Arrays.toString(o.decode(new int[]{6,5,4,6})));
+    }
+    public int[] decode(int[] encoded) {
+        int[] src = new int[encoded.length+1];
+        int xorSum = 0;
+        for (int i = 1; i <= encoded.length + 1; i++) {
+            xorSum ^= i;
+        }
+        for (int i = 0; i < encoded.length; i = i+2) {
+            xorSum ^= encoded[i+1];
+        }
+        src[0] = xorSum;
+        for (int i = 1; i < src.length; i++) {
+            src[i] = encoded[i-1] ^ src[i-1];
+        }
+        return src;
+    }
+}
