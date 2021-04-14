dbfe498a06178aec9256003911adb7facbbff502
ouyangyameng <ouyangyameng@vip.qq.com>
2021-04-14 19:55:57 +0800

    1124.LongestWellPerformingInterval

---
+++ leetcode/src/LongestWellPerformingInterval1124.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+/**
+ * @Author: ouyangyameng
+ * @Date: 2021/4/14 1:44 下午
+ * @Version 1.0
+ */
+//TODO 优化
+public class LongestWellPerformingInterval1124 {
+    public int longestWPI(int[] hours) {
+        int max = 0;
+        for (int i = 0 ; i < hours.length; i++) {
+            int cur = 0;
+            int curMax = 0;
+            for (int j = i; j < hours.length; j++) {
+                if(hours[j] > 8) {
+                    cur++;
+                } else {
+                    cur--;
+                }
+                if (cur > 0) {
+                    curMax = j - i + 1;
+                }
+            }
+            max = Math.max(curMax,max);
+        }
+        return max;
+    }
+}
