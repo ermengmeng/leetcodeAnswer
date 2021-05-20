3174db005a6ef49992b7b3fffb6685fd85bfe028
ouyangyameng <ouyangyameng@vip.qq.com>
2021-05-20 14:39:35 +0800

    413. Arithmetic Slices

---
+++ leetcode/src/ArithmeticSlices413.java
@@ -0,0 +1,31 @@
+/**
+ * @Author: ouyangyameng
+ * @Date: 2021/5/20 2:33 下午
+ * @Version 1.0
+ */
+public class ArithmeticSlices413 {
+    public static void main(String[] args) {
+        ArithmeticSlices413 o = new ArithmeticSlices413();
+    }
+
+    public int numberOfArithmeticSlices(int[] nums) {
+        if (nums.length < 3) {
+            return 0;
+        }
+        int curNum = 2;
+        int sum = 0;
+        int Dvalue = nums[1] - nums[0];
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] - nums[i - 1] == Dvalue) {
+                curNum++;
+                if (curNum >= 3) {
+                    sum += curNum - 2;
+                }
+            } else {
+                Dvalue = nums[i] - nums[i - 1];
+                curNum = 2;
+            }
+        }
+        return sum;
+    }
+}
