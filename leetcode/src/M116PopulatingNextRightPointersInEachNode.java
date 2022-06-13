b03d9df0fee17b6db992e2871e3f21e4383707df
ouyangyameng <ouyangyameng@sinaft.com>
2022-06-13 19:23:31 +0800

    M116&M117解法

--- leetcode/src/M116PopulatingNextRightPointersInEachNode.java
+++ leetcode/src/M116PopulatingNextRightPointersInEachNode.java
@@ -5,7 +5,13 @@ import common.Node;
  * @Date: 2022/6/13 14:02
  * @Version 1.0
  */
+
+
 public class M116PopulatingNextRightPointersInEachNode {
+    /**
+     * 因为是满树 所以使用root节点及root.next节点就可以处理完当前节点和下一个节点的链接。
+     * 因此左右树递归DFS处理所有子树即可
+     */
     public Node connect(Node root) {
         if (root == null || root.left == null) {
             return root;
--- leetcode/src/M117PopulatingNextRightPointersInEachNodeII.java
+++ leetcode/src/M117PopulatingNextRightPointersInEachNodeII.java
@@ -5,10 +5,16 @@ import common.Node;
  * @Date: 2022/6/13 18:44
  * @Version 1.0
  */
-public class M117PopulatingNextRightPointersInEachNodeII {
-    public static void main(String[] args) {
 
-    }
+public class M117PopulatingNextRightPointersInEachNodeII {
+    /**
+     * 由于题目中指出不是满树
+     * 因此当前节点的next节点无法根据,root的next链得出(DFS过程中可能还没处理到后面的next)
+     * 向前保存需要额外的空间记录每层的最后一个last
+     * 因此采用BFS处理
+     * @param root
+     * @return
+     */
     public Node connect(Node root) {
         Node cur = root;
         Node leftest = null;
