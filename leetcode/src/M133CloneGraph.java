f8ece444cad99b479c8f7b350aa9922963449bab
ouyangyameng <ouyangyameng@sinaft.com>
2022-06-20 17:47:04 +0800

    M133

--- leetcode/src/M133CloneGraph.java
+++ leetcode/src/M133CloneGraph.java
@@ -11,29 +11,37 @@ public class M133CloneGraph {
     public static void main(String[] args) {
 
     }
+
+    /**
+     * 普通的bfs
+     * 遍历每一层没处理过的节点,处理单向的neighbors信息
+     * 少量紧凑的整数key，用数组下标代替map的key取值效率高很多。
+     * 具体看题目要求的key值域是否合适映射成紧凑的下标。
+     * @param node
+     * @return
+     */
     public GraphNode cloneGraph(GraphNode node) {
         if (node == null) {
             return null;
         }
-        Map<Integer, GraphNode> map = new HashMap<>();
+        GraphNode[] visited = new GraphNode[101];
         List<GraphNode> list = Arrays.asList(node);
         while (list.size() > 0) {
             List<GraphNode> next = new ArrayList<>();
             for (GraphNode cur : list) {
-                if (!map.containsKey(cur.val)) {
-                    map.put(cur.val, new GraphNode(cur.val));
+                if (visited[cur.val] == null) {
+                    visited[cur.val] =  new GraphNode(cur.val);
                 }
-                GraphNode curClone = map.get(cur.val);
                 for (GraphNode neighbor : cur.neighbors) {
-                    if (!map.containsKey(neighbor.val)) {
-                        map.put(neighbor.val, new GraphNode(neighbor.val));
+                    if (visited[neighbor.val] == null) {
+                        visited[neighbor.val] = new GraphNode(neighbor.val);
                         next.add(neighbor);
                     }
-                    curClone.neighbors.add(map.get(neighbor.val));
+                    visited[cur.val].neighbors.add(visited[neighbor.val]);
                 }
             }
             list = next;
         }
-        return map.get(1);
+        return visited[1];
     }
 }
