2bb4b42669295a9822d06dda8647c7ff5e9850f5
ouyangyameng <ouyangyameng@sinaft.com>
2022-06-21 18:32:03 +0800

    M137&M820

--- leetcode/src/M137SingleNumber2.java
+++ leetcode/src/M137SingleNumber2.java
@@ -5,28 +5,19 @@
  */
 public class M137SingleNumber2 {
     public static void main(String[] args) {
-        System.out.println(new M137SingleNumber2().singleNumber(new int[]{1,1,1,2}));
+        System.out.println(new M137SingleNumber2().singleNumber(new int[]{1, 1, 1, 2}));
     }
 
     public int singleNumber(int[] nums) {
-        int[] bitNum = new int[32];
-        int[] pow = new int[32];
-        int base = 1;
+        int base = 0;
         for (int i = 0; i < 32; i++) {
-            pow[i] = base;
-            base = base << 1;
-        }
-        for (int num : nums) {
-            for (int i = 0; i < 32; i++) {
-                if ((num & pow[i]) != 0) {
-                    bitNum[i] = (bitNum[i] + 1) % 3;
+            int sum = 0;
+            for (int num : nums) {
+                if ((num >> i & 1) == 1) {
+                    sum = (sum + 1) % 3;
                 }
             }
-        }
-        base = 0;
-        for (int i = 31; i >= 0 ; i--) {
-            base = base << 1;
-            base = (base | bitNum[i]);
+            base += sum << i;
         }
         return base;
     }
